845e03d91558df95894ff8a4650bec060d01e145
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2014-06-06 06:07:40 +0900

    add setup_vim.sh

---
+++ setup_vim.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+type git >/dev/null 2>&1 || ( echo "git command not found" && exit 1 )
+
+curl -o ~/.vimrc https://raw.githubusercontent.com/yosukeota/dotfiles/master/.vimrc >/dev/null 2>&1 || echo "Could not download .vimrc"
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle
+[ ! -d ~/.vim/bundle/neobundle ] && git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+[ ! -d ~/.vim/bundle/vimproc ] && git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
+
+echo "Please run this command: $ vim -c ':NeoBundleInstall'"
