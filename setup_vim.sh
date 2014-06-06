ae321f9d88ecda1f4b6f527a492a92dbc29c3e12
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2014-06-06 20:03:36 +0900

    give color to echo command

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-type git >/dev/null 2>&1 || ( echo "git: command not found" && exit 1 )
-type ctags >/dev/null 2>&1 || ( echo "ctags: command not found" && exit 1 )
-echo "If you use Mac, you should install exuberant ctags."
-echo "Run following command."
-echo "$ brew install ctags"
+type git >/dev/null 2>&1 || ( echo '\033[31mgit: command not found\033[0m' && exit 1 )
+type ctags >/dev/null 2>&1 || ( echo '\033[31mctags: command not found\033[0m' && exit 1 )
+echo 'You should install exuberant ctags.'
+echo 'Run following command.'
+echo 'Mac:    \033[33m$ brew install ctags\033[0m'
+echo '     or \033[33m$ port install ctags\033[0m'
+echo 'RedHat: \033[33m$ yum install ctags\033[0m'
+echo 'Debian: \033[33m$ apt-get install ctags\033[0m'
 
-curl -o ~/.vimrc https://raw.githubusercontent.com/yosukeota/dotfiles/master/.vimrc >/dev/null 2>&1 || ( echo "Could not download .vimrc" && exit 1 )
+curl -o ~/.vimrc https://raw.githubusercontent.com/yosukeota/dotfiles/master/.vimrc >/dev/null 2>&1 || ( echo '\033[31mCould not download .vimrc\033[0m' && exit 1 )
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle
-[ ! -d ~/.vim/bundle/neobundle.vim ] && git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim >/dev/null 2>&1 || ( echo "failed git clone neobundle.vim." && exit 1 )
+[ ! -d ~/.vim/bundle/neobundle.vim ] && git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim >/dev/null 2>&1 || ( echo '\033[31mfailed git clone neobundle.vim.\033[0m' && exit 1 )
 
-echo "\nPlease run following command: $ vim -c ':NeoBundleInstall'"
+echo '\nPlease run following command: \033[33m$ vim -c ':NeoBundleInstall'\033[0m'
