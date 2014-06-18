eb73d0aad8c33db2146367920bda8ac8351daf66
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2014-06-18 13:50:22 +0900

    Use echo -e in setup_vim.sh
    Please see following url:
    http://mattintosh.hatenablog.com/entry/2013/12/11/201503

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-type git >/dev/null 2>&1 || ( echo "\033[31mgit: command not found\033[0m" && exit 1 ) || exit 1
-type ctags >/dev/null 2>&1 || echo "\033[31mctags: command not found\033[0m"
+type git >/dev/null 2>&1 || ( echo -e "\033[31mgit: command not found\033[0m" && exit 1 ) || exit 1
+type ctags >/dev/null 2>&1 || echo -e "\033[31mctags: command not found\033[0m"
 
 if [ -f ~/.vimrc ]; then
   /bin/echo -n "Already exist ~/.vimrc, backup this file? [y/n]: "
@@ -13,21 +13,21 @@ if [ -f ~/.vimrc ]; then
   elif [ $response == "n" ]; then
     echo "Overwrite ~/.vimrc.\n"
   else
-    echo "\033[31mError: Please input y or n\033[0m"
+    echo -e "\033[31mError: Please input y or n\033[0m"
     exit 1
   fi
 fi
 
 echo "You should install exuberant ctags."
 echo "Run following command."
-echo "Mac:    \033[33m$ brew install ctags\033[0m"
-echo "     or \033[33m$ port install ctags\033[0m"
-echo "RedHat: \033[33m$ yum install ctags\033[0m"
-echo "Debian: \033[33m$ apt-get install ctags\033[0m"
+echo -e "Mac:    \033[33m$ brew install ctags\033[0m"
+echo -e "     or \033[33m$ port install ctags\033[0m"
+echo -e "RedHat: \033[33m$ yum install ctags\033[0m"
+echo -e "Debian: \033[33m$ apt-get install ctags\033[0m"
 
-curl -o ~/.vimrc https://raw.githubusercontent.com/yosukeota/dotfiles/master/.vimrc >/dev/null 2>&1 || ( echo "\033[31mCould not download .vimrc\033[0m" && exit 1 ) || exit 1
+curl -o ~/.vimrc https://raw.githubusercontent.com/yosukeota/dotfiles/master/.vimrc >/dev/null 2>&1 || ( echo -e "\033[31mCould not download .vimrc\033[0m" && exit 1 ) || exit 1
 
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle
-[ -d ~/.vim/bundle/neobundle.vim ] && echo "\n~/.vim/bundle/neobundle.vim is already exist." || git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim >/dev/null 2>&1 || echo "\033[31mgit clone failed\033[0m"
+[ -d ~/.vim/bundle/neobundle.vim ] && echo "\n~/.vim/bundle/neobundle.vim is already exist." || git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim >/dev/null 2>&1 || echo -e "\033[31mgit clone failed\033[0m"
 
-echo "\nPlease run following command: \033[33m$ vim -c ":NeoBundleInstall"\033[0m"
+echo -e "\nPlease run following command: \033[33m$ vim -c ":NeoBundleInstall"\033[0m"
